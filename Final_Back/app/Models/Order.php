dfd3850564ced8ca80c7f72f5f99f3a6118fed7e
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-04-28 15:51:10 +0200

    feat: added a relations by Order model

--- Final_Back/app/Models/Order.php
+++ Final_Back/app/Models/Order.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    
+    public $fillable = [
+        'order_id',
+        'user_id',
+        'product_id',
+        'order_date',
+    ];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
